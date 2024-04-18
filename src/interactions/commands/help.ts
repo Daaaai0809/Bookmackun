5ac20de646929df8ddf46cd8021fc4062752b94d
Daaaai0809 <dai.tsuruga@80and.co>
2024-04-19 00:58:28 +0900

    chore: modify response messages

--- src/interactions/commands/help.ts
+++ src/interactions/commands/help.ts
@@ -3,11 +3,11 @@ import { buildHelpCommandResponse } from "@/response/help_command_response";
 
 const content = () => `
     使い方 \n
-\`/add $URL\`: ブックマークにURLを追加します \n
-\`/list\`: ブックマークの一覧を表示します \n
-\`/read $ID\`: ブックマークを既読にします \n
-\`/remove $ID\`: ブックマークを削除します \n
-\`/help\`: ヘルプを表示します \n
+\`/bm add $URL\`: ブックマークにURLを追加します \n
+\`/bm list\`: ブックマークの一覧を表示します \n
+\`/bm read $ID\`: ブックマークを既読にします \n
+\`/bm remove $ID\`: ブックマークを削除します \n
+\`/bm help\`: ヘルプを表示します \n
 `;
 
 const handler = async () => {
